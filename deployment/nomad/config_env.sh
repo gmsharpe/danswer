2e5e6ac8eea633bf0a9bac4b37283287039a2ef7
gmsharpe <gmsharpe@ucdavis.edu>
2024-09-09 15:35:41 -0700

    no message

--- deployment/nomad/config_env.sh
+++ deployment/nomad/config_env.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# install git
+sudo yum install git -y
+
+# clone the repository
+sudo git clone https://github.com/gmsharpe/danswer.git
+
 # create the directories for the nomad volumes
 mkdir -p /var/nomad/volumes/danswer
 cd /var/nomad/volumes/danswer && mkdir -p db_volume vespa_volume
+
+# copy the ngnix files
+cp -r /home/ec2-user/danswer/data/nginx /var/nomad/volumes/danswer/nginx
