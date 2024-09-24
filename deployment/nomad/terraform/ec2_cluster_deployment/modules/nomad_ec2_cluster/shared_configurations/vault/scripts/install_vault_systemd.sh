8b222088f0292f63192f3ae95d116db660901f4d
gmsharpe <gmsharpe@ucdavis.edu>
2024-09-24 13:35:18 -0700

    no message

--- deployment/nomad/terraform/ec2_cluster_deployment/modules/nomad_ec2_cluster/shared_configurations/vault/scripts/install_vault_systemd.sh
+++ deployment/nomad/terraform/ec2_cluster_deployment/modules/nomad_ec2_cluster/shared_configurations/vault/scripts/install_vault_systemd.sh
@@ -9,9 +9,9 @@ SYSTEMD_DIR="/etc/systemd/system"
 echo "Installing systemd services for RHEL/CentOS"
 
 sudo cp ./vault/init/systemd/vault.service ${SYSTEMD_DIR}/vault.service
-sudo cp ./vault/init/systemd/consul-online.service ${SYSTEMD_DIR}/consul-online.service
-sudo cp ./vault/init/systemd/consul-online.target ${SYSTEMD_DIR}/consul-online.target
-sudo cp ./vault/init/systemd/consul-online.sh ${SYSTEMD_DIR}/consul-online.sh
+sudo cp ./consul/init/systemd/consul-online.service ${SYSTEMD_DIR}/consul-online.service
+sudo cp ./consul/init/systemd/consul-online.target ${SYSTEMD_DIR}/consul-online.target
+sudo cp ./consul/init/systemd/consul-online.sh ${SYSTEMD_DIR}/consul-online.sh
 
 sudo chmod 0664 ${SYSTEMD_DIR}/{vault*,consul*}
 
