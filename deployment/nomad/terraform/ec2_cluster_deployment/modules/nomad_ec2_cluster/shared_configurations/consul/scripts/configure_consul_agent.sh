409aa3628f746df0563fe584590fb6db5bbbbbdc
gmsharpe <gmsharpe@ucdavis.edu>
2024-09-26 12:48:10 -0700

    no message

--- deployment/nomad/terraform/ec2_cluster_deployment/modules/nomad_ec2_cluster/shared_configurations/consul/scripts/configure_consul_agent.sh
+++ deployment/nomad/terraform/ec2_cluster_deployment/modules/nomad_ec2_cluster/shared_configurations/consul/scripts/configure_consul_agent.sh
@@ -13,7 +13,7 @@ CONSUL_CONFIG_DIR=/etc/consul.d
 CONSUL_ENV_VARS=$CONSUL_CONFIG_DIR/consul.conf
 
 # If override is true, use the custom config if set; otherwise, use the default config file
-if [ ${DO_OVERRIDE_CONFIG} == true ] && [ -z "${CONSUL_OVERRIDE_CONFIG}" ]; then
+if [ ${DO_OVERRIDE_CONFIG} == true ]; then
   echo "Use custom Consul agent config (CONSUL_OVERRIDE_CONFIG)"
   CONSUL_CONFIG=${CONSUL_OVERRIDE_CONFIG}
 else
